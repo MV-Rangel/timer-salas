fa7e12240d2dc215a9b1cdbdcd86c29151905933
Marcus <mrangelpedagogo@gmail.com>
2024-12-04 21:34:29 -0300

    Changes

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
-const form = document.querySelector('#reservationForm')
+const form = document.querySelector("#reservationForm");
 
-const checkFormFieldsValues = ()=>{
-    const student_name = document.querySelector("#student_name")
-    form.addEventListener('submit',(e)=>{
-        e.preventDefault();
-        console.log(student_name.value)
-       
-    })
-}
+const checkFormFieldsValues = () => {
+  const reservations = document.querySelector("#reservations");
+  const student_name = document.querySelector("#student_name");
+  const registration = document.querySelector("#registration");
+  const room = document.querySelector("#room");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const createSpan = (className, textContent) => {
+      const span = document.createElement("span");
+      span.className = className;
+      span.textContent = textContent;
+      return span;
+    };
+
+    const paragraph = document.createElement("p");
+
+    paragraph.appendChild(document.createTextNode("Sala "));
+    paragraph.appendChild(createSpan("room_number", room.value));
+    paragraph.appendChild(document.createTextNode(" reservada para "));
+    paragraph.appendChild(createSpan("reserved_to", student_name.value));
+    paragraph.appendChild(document.createTextNode(" - "));
+    paragraph.appendChild(
+      createSpan("student_registration", registration.value)
+    );
+
+    reservations.appendChild(paragraph);
+    form.reset();
+  });
+  const cancel_button = document.querySelector(".cancel_button");
+  cancel_button.addEventListener("click", () => {
+    form.reset();
+  });
+};
 checkFormFieldsValues();
